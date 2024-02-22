c4f02e92cdd969b1bb23ba551e2258d174042bc2
Wiscarlens <wiscarlens@gmail.com>
2024-02-22 05:39:20 -0500

    add AddingTrouble

---
+++ AddingTrouble.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class AddingTrouble {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int firstDigit = scanner.nextInt();
+        int secondDigit = scanner.nextInt();
+        int result = scanner.nextInt();
+
+        boolean isCorrect = firstDigit + secondDigit == result;
+
+        if(isCorrect) {
+            System.out.println("correct!");
+        } else {
+            System.out.println("wrong!");
+        }
+
+    }
+}
